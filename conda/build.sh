2ed07f27df5763d5ebc5be1da318b7fc28a6475b
guacke <Guillaume.Acke@UGent.be>
2020-09-03 17:34:55 +0200

    Enable Conda CI/CD for v2.4.2.

---
+++ conda/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+./autogen.sh
+mkdir build && cd build
+if [ `uname` == Darwin ]; then
+    ../configure \ 
+        --prefix=${PREFIX} \
+        --enable-shared=yes \ 
+        --enable-static=no \
+        CC=${CLANG} \
+        CXX=${CLANGXX} \
+        CFLAGS="${CFLAGS} ${OPTS}" \ 
+        CXXFLAGS="${CXXFLAGS} ${OPTS} -I${PREFIX}/include"
+fi
+if [ `uname` == Linux ]; then
+    ../configure \ 
+        --prefix=${PREFIX} \
+        --enable-shared=yes \ 
+        --enable-static=no \
+        CC=${GCC} \
+        CXX=${GXX} \
+        CFLAGS="${CFLAGS} ${OPTS}" \ 
+        CXXFLAGS="${CXXFLAGS} ${OPTS} -I${PREFIX}/include"
+fi
+
+make -j${CPU_COUNT} VERBOSE=1 && make check && make install
