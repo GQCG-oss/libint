8fb5298b87dd4c9c0bdf0505882f497ef3f08594
guacke <Guillaume.Acke@UGent.be>
2020-09-03 15:29:11 +0200

    Add include and lib dir of Boost.

--- conda/build.sh
+++ conda/build.sh
@@ -2,5 +2,5 @@
 
 ./autogen.sh
 mkdir build && cd build
-../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no CXXFLAGS="-O2 -std=c++11"
+../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no CXXFLAGS="-O2 -std=c++11 -I${PREFIX}/include/ -L${PREFIX}/lib -lboost"
 make -j${CPU_COUNT} VERBOSE=1 && make check && make install
