7fe58b5515c9162e4c3d1c851a03a7210dcb26b4
guacke <Guillaume.Acke@UGent.be>
2020-09-03 15:37:04 +0200

    Bugfix: typo.

--- conda/build.sh
+++ conda/build.sh
@@ -2,5 +2,5 @@
 
 ./autogen.sh
 mkdir build && cd build
-../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no CXXFLAGS="-O2 -std=c++11 -I${PREFIX}/include/ -L${PREFIX}/lib -lboost"
+../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no CXXFLAGS="-O2 -std=c++11" CPPFLAGS="-I${PREFIX}/include/ -L${PREFIX}/lib -lboost"
 make -j${CPU_COUNT} VERBOSE=1 && make check && make install
