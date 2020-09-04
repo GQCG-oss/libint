e63074d359e969a56ea17789606fb7e68f7a42d2
guacke <Guillaume.Acke@UGent.be>
2020-09-04 09:48:59 +0200

    Bugfix: do not use line continuation.

--- conda/build.sh
+++ conda/build.sh
@@ -4,32 +4,10 @@ set -x
 ./autogen.sh
 mkdir build && cd build
 if [ `uname` == Darwin ]; then
-    ../configure \ 
-        --prefix=${PREFIX} \
-        --enable-shared=yes \ 
-        --enable-static=no \
-        --with-incdirs=-I${PREFIX}/include \
-        --host=x86_64-apple-darwin19.6.0 \
-        --build=x86_64-apple-darwin19.6.0 \
-        --target=x86_64-apple-darwin19.6.0 \
-        CC=${CLANG} \
-        CXX=${CLANGXX} \
-        CFLAGS="${CFLAGS} ${OPTS}" \ 
-        CXXFLAGS="${CXXFLAGS} ${OPTS}"
+    ../configure --prefix=${PREFIX} --enable-shared=yes--enable-static=no --with-incdirs=-I${PREFIX}/include --host=x86_64-apple-darwin19.6.0 --build=x86_64-apple-darwin19.6.0 --target=x86_64-apple-darwin19.6.0 CC=${CLANG} CXX=${CLANGXX} CFLAGS="${CFLAGS} ${OPTS}" CXXFLAGS="${CXXFLAGS} ${OPTS}"
 fi
 if [ `uname` == Linux ]; then
-    ../configure \ 
-        --prefix=${PREFIX} \
-        --enable-shared=yes \ 
-        --enable-static=no \
-        --with-incdirs=-I${PREFIX}/include \
-        --host=x86_64-pc-linux-gnu \
-        --build=x86_64-pc-linux-gnu \
-        --target=x86_64-pc-linux-gnu \
-        CC=${GCC} \
-        CXX=${GXX} \
-        CFLAGS="${CFLAGS} ${OPTS} -fopenmp" \ 
-        CXXFLAGS="${CXXFLAGS} ${OPTS} -fopenmp"
+    ../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no --with-incdirs=-I${PREFIX}/include --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu CC=${GCC} CXX=${GXX} CFLAGS="${CFLAGS} ${OPTS} -fopenmp" CXXFLAGS="${CXXFLAGS} ${OPTS} -fopenmp"
 fi
 
 make -j ${CPU_COUNT}
