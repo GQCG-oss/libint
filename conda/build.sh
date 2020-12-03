d840ff3a5bc1eff3182715cb1d5d4c1277c6a044
guacke <Guillaume.Acke@UGent.be>
2020-12-03 15:52:07 +0100

    Debug: pass conda_build_sysroot to configure.

--- conda/build.sh
+++ conda/build.sh
@@ -4,16 +4,13 @@ set -x
 ./autogen.sh
 mkdir build && cd build
 if [ `uname` == Darwin ]; then
-    ../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no --with-incdirs=-I${PREFIX}/include --host=x86_64-apple-darwin19.6.0 --build=x86_64-apple-darwin19.6.0 --target=x86_64-apple-darwin19.6.0 CC=${CLANG} CXX=${CLANGXX} CFLAGS="${CFLAGS} ${OPTS}" CXXFLAGS="${CXXFLAGS} ${OPTS} -stdlib=libc++"
-
-    # forcibly link against libc++, rather than libstdc++, as LDFLAGS doesn't work
-    sed -i.bak 's;dynamiclib ;dynamiclib -stdlib=libc++ ;g' libtool
+    ../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no --with-incdirs=-I${PREFIX}/include --host=x86_64-apple-darwin19.6.0 --build=x86_64-apple-darwin19.6.0 --target=x86_64-apple-darwin19.6.0 CC=${CLANG} CXX=${CLANGXX} CFLAGS="${CFLAGS} ${OPTS} -isysroot ${CONDA_BUILD_SYSROOT}" CXXFLAGS="${CXXFLAGS} ${OPTS} -isysroot ${CONDA_BUILD_SYSROOT}"
 fi
 if [ `uname` == Linux ]; then
     ../configure --prefix=${PREFIX} --enable-shared=yes --enable-static=no --with-incdirs=-I${PREFIX}/include --host=x86_64-pc-linux-gnu --build=x86_64-pc-linux-gnu --target=x86_64-pc-linux-gnu CC=${GCC} CXX=${GXX} CFLAGS="${CFLAGS} ${OPTS} -fopenmp" CXXFLAGS="${CXXFLAGS} ${OPTS} -fopenmp"
 fi
 
 make -j ${CPU_COUNT}
-make check
 make install
+make check
 set +x
